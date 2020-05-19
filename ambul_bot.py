91920c2be0d3a71389e7344f12a31c25ca7e1bfc
Suresh Raju <sushraju@gmail.com>
2020-05-18 19:31:11 -0700

    pylint fixes

--- ambul_bot.py
+++ ambul_bot.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python
-import sys
-import tweepy
-from newsapi import NewsApiClient
-from datetime import datetime, timedelta
-import pytz
-from tweepy import TweepError
-
-from utils import *
 import logging
 import logging.handlers
+import sys
+from datetime import datetime
+import pytz
+import tweepy
+from newsapi import NewsApiClient
+from utils import get_config
+
+"""
+NewsBot uses twitter and newsapi
+"""
 
 
 class NewsBot(object):
 
     def __init__(self):
+        """
+        Initialize the twitter and news api using config data in json
+        """
         self.config_data = get_config()
 
         # initialize twitter api
-        auth = tweepy.OAuthHandler(self.config_data['auth']['api_key'], self.config_data['auth']['api_secret_key'])
-        auth.set_access_token(self.config_data['auth']['access_token'], self.config_data['auth']['access_token_secret'])
+        auth = tweepy.OAuthHandler(self.config_data['auth']['api_key'],
+                                   self.config_data['auth']['api_secret_key'])
+        auth.set_access_token(self.config_data['auth']['access_token'],
+                              self.config_data['auth']['access_token_secret'])
 
         self.twitter_api = tweepy.API(auth)
         try:
@@ -36,6 +43,8 @@ class NewsBot(object):
 
 
 def main():
+
+    # initialize logging
     logging.basicConfig(filename='ambul_bot.log', level=logging.INFO)
     ambul_bot = NewsBot()
 
@@ -44,6 +53,7 @@ def main():
     to_utc_time = pytz.UTC.localize(curr_utc_time)
     to_pst_time = to_utc_time.astimezone(pst)
 
+    # fetch news
     if ambul_bot.news_api:
         all_articles = ambul_bot.news_api.get_everything(sources=ambul_bot.news_sources,
                                                          from_param=str(to_pst_time.date()),
@@ -55,14 +65,15 @@ def main():
         logging.ERROR("Error while accessing news api, will try next time.")
         sys.exit(1)
 
+    # tweet by iterating the articles list
     for article in all_articles['articles']:
         if ambul_bot.twitter_api:
             try:
                 ambul_bot.twitter_api.update_status(article['url'])
-            except tweepy.TweepError as t:
+            except tweepy.TweepError:
                 continue
         else:
-            logging.WARN("Error accessing twitter API")
+            logging.WARNING("Error accessing twitter API")
 
 
 if __name__ == "__main__":
